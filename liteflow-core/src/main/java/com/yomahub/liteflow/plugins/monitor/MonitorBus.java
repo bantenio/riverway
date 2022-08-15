b097e5d94f00dd2277c7b140481e21fb361d2c88
sunkaihan <sunkaihan@weshare.com.cn>
2022-08-15 13:49:14 +0800

    添加插件支持

--- liteflow-core/src/main/java/com/yomahub/liteflow/plugins/monitor/MonitorBus.java
+++ liteflow-core/src/main/java/com/yomahub/liteflow/plugins/monitor/MonitorBus.java
@@ -1,6 +1,7 @@
 /**
  * <p>Title: liteflow</p>
  * <p>Description: 轻量级的组件式流程框架</p>
+ *
  * @author Bryan.Zhang
  * @email weenyc31@163.com
  * @Date 2020/4/1
@@ -34,102 +35,102 @@ import java.util.concurrent.TimeUnit;
  */
 public class MonitorBus implements NodeComponentExecuteInterceptor {
 
-	private LiteFlowConfig liteflowConfig;
+    private LiteFlowConfig liteflowConfig;
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	private final ConcurrentHashMap<String, BoundedPriorityBlockingQueue<CompStatistics>> statisticsMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, BoundedPriorityBlockingQueue<CompStatistics>> statisticsMap = new ConcurrentHashMap<>();
 
-	private final ScheduledExecutorService printLogScheduler = Executors.newScheduledThreadPool(1);
+    private final ScheduledExecutorService printLogScheduler = Executors.newScheduledThreadPool(1);
 
-	private MonitorProperties monitorProperties;
+    private MonitorProperties monitorProperties;
 
-	public MonitorBus(LiteFlowConfig liteflowConfig) {
-		this.liteflowConfig = liteflowConfig;
-		this.monitorProperties = liteflowConfig.getMonitorProperties();
+    public MonitorBus(LiteFlowConfig liteflowConfig) {
+        this.liteflowConfig = liteflowConfig;
+        this.monitorProperties = liteflowConfig.getMonitorProperties();
 
-		if(BooleanUtil.isTrue(liteflowConfig.getLogConfig().getEnableLog())){
-			this.printLogScheduler.scheduleAtFixedRate(new MonitorTimeTask(this), monitorProperties.getDelay(), monitorProperties.getPeriod(), TimeUnit.MILLISECONDS);
-		}
-	}
+        if (BooleanUtil.isTrue(liteflowConfig.getLogConfig().getEnableLog())) {
+            this.printLogScheduler.scheduleAtFixedRate(new MonitorTimeTask(this), monitorProperties.getDelay(), monitorProperties.getPeriod(), TimeUnit.MILLISECONDS);
+        }
+    }
 
-	public void addStatistics(CompStatistics statistics){
-		if(statisticsMap.containsKey(statistics.getComponentClazzName())){
-			statisticsMap.get(statistics.getComponentClazzName()).add(statistics);
-		}else{
-			BoundedPriorityBlockingQueue<CompStatistics> queue = new BoundedPriorityBlockingQueue<>(monitorProperties.getQueueLimit());
-			queue.offer(statistics);
-			statisticsMap.put(statistics.getComponentClazzName(), queue);
-		}
-	}
+    public void addStatistics(CompStatistics statistics) {
+        if (statisticsMap.containsKey(statistics.getComponentClazzName())) {
+            statisticsMap.get(statistics.getComponentClazzName()).add(statistics);
+        } else {
+            BoundedPriorityBlockingQueue<CompStatistics> queue = new BoundedPriorityBlockingQueue<>(monitorProperties.getQueueLimit());
+            queue.offer(statistics);
+            statisticsMap.put(statistics.getComponentClazzName(), queue);
+        }
+    }
 
-	public void printStatistics(){
-		try{
-			Map<String, BigDecimal> compAverageTimeSpent = new HashMap<String, BigDecimal>();
-			
-			for(Entry<String, BoundedPriorityBlockingQueue<CompStatistics>> entry : statisticsMap.entrySet()){
-				long totalTimeSpent = 0;
-				for(CompStatistics statistics : entry.getValue()){
-					totalTimeSpent += statistics.getTimeSpent();
-				}
-				compAverageTimeSpent.put(entry.getKey(), new BigDecimal(totalTimeSpent).divide(new BigDecimal(entry.getValue().size()), 2, RoundingMode.HALF_UP));
-			}
+    public void printStatistics() {
+        try {
+            Map<String, BigDecimal> compAverageTimeSpent = new HashMap<String, BigDecimal>();
 
-			List<Entry<String, BigDecimal>> compAverageTimeSpentEntryList = new ArrayList<>(compAverageTimeSpent.entrySet());
+            for (Entry<String, BoundedPriorityBlockingQueue<CompStatistics>> entry : statisticsMap.entrySet()) {
+                long totalTimeSpent = 0;
+                for (CompStatistics statistics : entry.getValue()) {
+                    totalTimeSpent += statistics.getTimeSpent();
+                }
+                compAverageTimeSpent.put(entry.getKey(), new BigDecimal(totalTimeSpent).divide(new BigDecimal(entry.getValue().size()), 2, RoundingMode.HALF_UP));
+            }
 
-			Collections.sort(compAverageTimeSpentEntryList, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+            List<Entry<String, BigDecimal>> compAverageTimeSpentEntryList = new ArrayList<>(compAverageTimeSpent.entrySet());
 
-			StringBuilder logStr = new StringBuilder();
-			logStr.append("以下为LiteFlow中间件统计信息：\n");
-			logStr.append("======================================================================================\n");
-			logStr.append("===================================SLOT INFO==========================================\n");
-			logStr.append(MessageFormat.format("SLOT TOTAL SIZE : {0}\n", liteflowConfig.getSlotSize()));
-			logStr.append(MessageFormat.format("SLOT OCCUPY COUNT : {0}\n", DataBus.OCCUPY_COUNT));
-			logStr.append("===============================TIME AVERAGE SPENT=====================================\n");
-			for(Entry<String, BigDecimal> entry : compAverageTimeSpentEntryList){
-				logStr.append(MessageFormat.format("COMPONENT[{0}] AVERAGE TIME SPENT : {1}\n", entry.getKey(), entry.getValue()));
-			}
-			logStr.append("======================================================================================\n");
-			log.info(logStr.toString());
-		}catch(Exception e){
-			log.error("print statistics cause error",e);
-		}
-	}
+            Collections.sort(compAverageTimeSpentEntryList, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
 
-	public LiteFlowConfig getLiteflowConfig() {
-		return liteflowConfig;
-	}
+            StringBuilder logStr = new StringBuilder();
+            logStr.append("以下为LiteFlow中间件统计信息：\n");
+            logStr.append("======================================================================================\n");
+            logStr.append("===================================SLOT INFO==========================================\n");
+            logStr.append(MessageFormat.format("SLOT TOTAL SIZE : {0}\n", liteflowConfig.getSlotSize()));
+            logStr.append(MessageFormat.format("SLOT OCCUPY COUNT : {0}\n", DataBus.OCCUPY_COUNT));
+            logStr.append("===============================TIME AVERAGE SPENT=====================================\n");
+            for (Entry<String, BigDecimal> entry : compAverageTimeSpentEntryList) {
+                logStr.append(MessageFormat.format("COMPONENT[{0}] AVERAGE TIME SPENT : {1}\n", entry.getKey(), entry.getValue()));
+            }
+            logStr.append("======================================================================================\n");
+            log.info(logStr.toString());
+        } catch (Exception e) {
+            log.error("print statistics cause error", e);
+        }
+    }
 
-	public void setLiteflowConfig(LiteFlowConfig liteflowConfig) {
-		this.liteflowConfig = liteflowConfig;
-	}
+    public LiteFlowConfig getLiteflowConfig() {
+        return liteflowConfig;
+    }
 
-	public void closeScheduler(){
-		this.printLogScheduler.shutdown();
-	}
+    public void setLiteflowConfig(LiteFlowConfig liteflowConfig) {
+        this.liteflowConfig = liteflowConfig;
+    }
 
-	@Override
-	public Object initContext(InterceptorContext interceptorContext) {
-		return null;
-	}
+    public void closeScheduler() {
+        this.printLogScheduler.shutdown();
+    }
 
-	@Override
-	public void beforeProcess(NodeComponentInterceptorContext interceptorContext) {
+    @Override
+    public Object initContext(InterceptorContext interceptorContext) {
+        return null;
+    }
 
-	}
+    @Override
+    public void beforeProcess(NodeComponentInterceptorContext interceptorContext) {
 
-	@Override
-	public void onSuccess(NodeComponentInterceptorContext interceptorContext) {
+    }
 
-	}
+    @Override
+    public void onSuccess(NodeComponentInterceptorContext interceptorContext) {
 
-	@Override
-	public void onError(NodeComponentInterceptorContext interceptorContext) {
+    }
 
-	}
+    @Override
+    public void onError(NodeComponentInterceptorContext interceptorContext) {
 
-	@Override
-	public void onFinally(NodeComponentInterceptorContext interceptorContext) {
+    }
 
-	}
+    @Override
+    public void onFinally(NodeComponentInterceptorContext interceptorContext) {
+        addStatistics(new CompStatistics(interceptorContext.getNodeComponent().getClass().getName(), interceptorContext.getCmpStep().getTimeSpent()));
+    }
 }
